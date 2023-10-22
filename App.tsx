f8ad8510efb6e86cdab287ed58681b8694c27b1b
Phạm Quang Vinh <phamquangvinh13062003@gmail.com>
2023-10-22 18:10:52 +0700

    Update FE

--- App.tsx
+++ App.tsx
@@ -5,6 +5,10 @@ import Login from './src/screens/Login'
 import Home from './src/screens/Home'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Notification from './src/screens/Notification'
+import PayMode from './src/screens/PayMode'
+import PaySuccess from './src/screens/PaySuccess'
+import PayLosing from './src/screens/PayLosing'
 
 
 
@@ -12,13 +16,17 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }}/>
+    //     <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
+    //     <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    // <Notification />
+    // <PayMode/>
+    <PayLosing />
+
   )
 }
 
