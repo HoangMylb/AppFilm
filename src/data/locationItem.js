b756537c96e39f21f4734727070ff8d28c7b497b
PhucPham1410 <pvp14102003@gmail.com>
2023-10-25 21:47:19 +0700

    Update Location

--- src/data/locationItem.js
+++ src/data/locationItem.js
@@ -5,6 +5,24 @@ const locationList = [
       address: '116 Nguyễn Du, Quận 1, TP.HCM',
       phone: '1900 2224'
     },
+    {
+      url: 'https://firebasestorage.googleapis.com/v0/b/fir-cinemaapp-dcbcf.appspot.com/o/CVNguyenDu.png?alt=media&token=807c6e2f-7058-4ab8-b458-7b0be3633970&_gl=1*c8p7oa*_ga*MTI1MTA0NjU4MC4xNjg5OTM2NTk0*_ga_CW55HF8NVT*MTY5Njk1OTcyMy4xMS4xLjE2OTY5NTk4MjguMjguMC4w',
+      title: 'CV Nguyễn Du',
+      address: '116 Nguyễn Du, Quận 1, TP.HCM',
+      phone: '1900 2224'
+    },
+    {
+      url: 'https://firebasestorage.googleapis.com/v0/b/fir-cinemaapp-dcbcf.appspot.com/o/CVNguyenDu.png?alt=media&token=807c6e2f-7058-4ab8-b458-7b0be3633970&_gl=1*c8p7oa*_ga*MTI1MTA0NjU4MC4xNjg5OTM2NTk0*_ga_CW55HF8NVT*MTY5Njk1OTcyMy4xMS4xLjE2OTY5NTk4MjguMjguMC4w',
+      title: 'CV Nguyễn Du',
+      address: '116 Nguyễn Du, Quận 1, TP.HCM',
+      phone: '1900 2224'
+    },
+    {
+      url: 'https://firebasestorage.googleapis.com/v0/b/fir-cinemaapp-dcbcf.appspot.com/o/CVNguyenDu.png?alt=media&token=807c6e2f-7058-4ab8-b458-7b0be3633970&_gl=1*c8p7oa*_ga*MTI1MTA0NjU4MC4xNjg5OTM2NTk0*_ga_CW55HF8NVT*MTY5Njk1OTcyMy4xMS4xLjE2OTY5NTk4MjguMjguMC4w',
+      title: 'CV Nguyễn Du',
+      address: '116 Nguyễn Du, Quận 1, TP.HCM',
+      phone: '1900 2224'
+    },
   ];
   
   export default locationList;
--- src/screens/Location.js
+++ src/screens/Location.js
@@ -1,130 +1,71 @@
-import React, { useState } from 'react'
-import { FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View, SafeAreaView } from 'react-native'
+import React, {useState} from 'react';
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+} from 'react-native';
 
 import locationList from '../data/locationItem';
 
-const Location = ({navigation}) => {
-    //data rạp 
-    const [location, setLocation] = useState(locationList);
+const Location = (props) => {
+  //data rạp
+  const [location, setLocation] = useState(locationList);
+  const {navigation} = props;
 
-    const renderItem = ({ item }) => {
-        return (
-            <View>
-                <Image
-                    style={{
-                        width: 100,
-                        height: 95,
-                        resizeMode: 'cover',
-                    }}
-                    source={{ uri: item.url }}
-                />
-                <Text
-                    style={{
-                        color: 'black',
-                        fontSize: 24,
-                        top: -90,
-                        left: 5,
-                        textAlign: 'center',
-                    }}>
-                    {item.title}
-                </Text>
-                <Text
-                    style={{
-                        color: 'black',
-                        fontSize: 16,
-                        left: 45,
-                        top: -80,
-                        textAlign: 'center',
-                    }}>
-                    {item.address}
-                </Text>
-                <Text
-                    style={{
-                        color: 'black',
-                        fontSize: 16,
-                        left: -30,
-                        top: -70,
-                        textAlign: 'center',
-                    }}>
-                    {item.phone}
-                </Text>
-            </View>
-        )
-    }
-
+  const renderItem = ({item}) => {
+    const {url, title, address, phone, id} = item;
     return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView>
-                <View style={styles.header}>
-                    <FlatList
-                        style={{ flex: 1 }}
-                        data={location}
-                        keyExtractor={item => item.title}
-                        renderItem={renderItem}
-                    />
-                </View>
-                <Text style={{top: -10}}>______________________________________________________</Text>
-                <View style={styles.header}>
-                    <FlatList
-                        style={{ flex: 1 }}
-                        data={location}
-                        keyExtractor={item => item.title}
-                        renderItem={renderItem}
-                    />
-                </View>
-                <Text style={{top: -10}}>______________________________________________________</Text>
-                <View style={styles.header}>
-                    <FlatList
-                        style={{ flex: 1 }}
-                        data={location}
-                        keyExtractor={item => item.title}
-                        renderItem={renderItem}
-                    />
-                </View>
-                <Text style={{top: -10}}>______________________________________________________</Text>
-                <View style={styles.header}>
-                    <FlatList
-                        style={{ flex: 1 }}
-                        data={location}
-                        keyExtractor={item => item.title}
-                        renderItem={renderItem}
-                    />
-                </View>
-                <Text style={{top: -10}}>______________________________________________________</Text>
-                <View style={styles.header}>
-                    <FlatList
-                        style={{ flex: 1 }}
-                        data={location}
-                        keyExtractor={item => item.title}
-                        renderItem={renderItem}
-                    />
-                </View>
-                <Text style={{top: -10}}>______________________________________________________</Text>
-                <View style={styles.header}>
-                    <FlatList
-                        style={{ flex: 1 }}
-                        data={location}
-                        keyExtractor={item => item.title}
-                        renderItem={renderItem}
-                    />
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
+      <View style={{marginTop: 10}}>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            style={{width: 100, height: 95, resizeMode: 'cover', borderRadius: 8}}
+            source={{uri: url}}
+          />
+          <View style={{padding: 5}}>
+            <Text style={{color: 'white', fontSize: 24}}> {title}</Text>
+            <Text style={{color: 'white', fontSize: 16, marginTop: 10}}>
+              {address}
+            </Text>
+            <Text style={{color: 'white', fontSize: 16, marginTop: 10}}>
+              {phone}
+            </Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            width: 400,
+            height: 1,
+            backgroundColor: 'black',
+            marginTop: 10,
+          }}></View>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={{flex: 1}}
+        data={location}
+        keyExtractor={(item, index) => item.name + index.toString()}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
+  );
 };
 
-export default Location
+export default Location;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 15,
-        backgroundColor: '#CF9B9B'
-    },
-    header:{
-        justifyContent: 'center',
-        backgroundColor: '#CF9B9B',
-        width: 399,
-        height: 106,
-    }
-})
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#18191A',
+  },
+});
